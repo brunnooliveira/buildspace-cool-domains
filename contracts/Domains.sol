fe7573ff9e1beca94c335aeaa2aba04f2cf4ec11
brunnooliveira <brunnogoncalves@gmail.com>
2022-08-22 22:27:43 -0300

    feat: adds records storage

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,19 +5,29 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
 
-  mapping(string => address) public domains;
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart");
+    }
 
-  constructor() {
-    console.log("THIS IS MY DOMAINS CONTRACT. NICE.");
-  }
+    function register(string calldata name) public {
+        require(domains[name] == address(0));
+        domains[name] = msg.sender;
+        console.log("%s has registered a domain!", msg.sender);
+    }
 
-   function register(string calldata name) public {
-      domains[name] = msg.sender;
-      console.log("%s has registered a domain!", msg.sender);
-  }
+    function getAddress(string calldata name) public view returns (address) {
+        return domains[name];
+    }
 
-  function getAddress(string calldata name) public view returns (address) {
-      return domains[name];
-  }
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender);
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name) public view returns (string memory) {
+        return records[name];
+    }
 }
