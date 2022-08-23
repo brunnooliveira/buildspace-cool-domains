32c0fba264027aaf7d4d69e7aae750ad8bb943ee
brunnooliveira <brunnogoncalves@gmail.com>
2022-08-22 22:26:36 -0300

    chore: add accounts task to list address

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,3 +4,12 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.10",
 };
+
+
+task("accounts", "Prints the list of accounts", async () => {
+  const accounts = await ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
